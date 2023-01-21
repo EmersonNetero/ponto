364af7dbe82e41a49f411c7cd274b04879294f15
Emersumm <emersonbk15@gmail.com>
2023-01-21 16:26:18 -0300

    feat: projeto api inicial

---
+++ api-ponto/Controllers/PontoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_ponto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class PontoController : ControllerBase
+    {
+
+        public PontoController()
+        {}
+        [HttpGet]
+        public IActionResult TestePonto()
+        {
+            return Ok("Ponto");
+        }
+    }
+}
