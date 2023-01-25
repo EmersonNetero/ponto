2c1de8dcc5aaf472c0700f35e707c6e8afc81248
Emersumm <emersonbk15@gmail.com>
2023-01-24 21:55:32 -0300

    feat: adiciona total horas

--- ponto-digital-front/src/components/Tabela.jsx
+++ ponto-digital-front/src/components/Tabela.jsx
@@ -1,14 +1,15 @@
 function ConvertDate(stringDate) {
-  const newDate = stringDate.split('-')
-  
-  let auxTrade = newDate[2].split('T')[0]
-  newDate[2] = newDate[0]
-  newDate[0] = auxTrade
-  return newDate.join('/')
+  const newDate = stringDate.split("-");
+
+  let auxTrade = newDate[2].split("T")[0];
+  newDate[2] = newDate[0];
+  newDate[0] = auxTrade;
+  return newDate.join("/");
 }
 
+export function Tabela({ result }) {
 
-export function Tabela({result}) {
+  let valor = 0;
   return (
     <div className="tabela-result">
       <table>
@@ -24,20 +25,31 @@ export function Tabela({result}) {
           </tr>
         </thead>
         <tbody>
-          
-            {result.map((ponto, i) => (
-              <tr>
-                <td>{ConvertDate(ponto.punchDate)}</td>
-                <td>{ponto.entries[0].punchDateTime.split("T")[1]}</td>
-                <td>{ponto.entries[1].punchDateTime.split("T")[1]}</td>
-                <td></td>
-                <td></td>
-                <td>{ponto.amountOfHoursWorked}</td>
-              </tr>
-            ))}
-          
+          {result.map((ponto, i) => (
+            <tr>
+              <td>{ConvertDate(ponto.punchDate)}</td>
+              <td>{ponto.entries[0].punchDateTime.split("T")[1]}</td>
+              <td>{ponto.entries[1].punchDateTime.split("T")[1]}</td>
+              <td></td>
+              <td></td>
+              <td>{ponto.amountOfHoursWorked}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <table className="tabela__total">
+        <thead>
+          <tr>
+            <th>TOTAL HS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{result.reduce((acc, curr) => acc + curr.amountOfHoursWorked, valor)}</td>
+          </tr>
         </tbody>
       </table>
     </div>
-  )
+  );
 }
